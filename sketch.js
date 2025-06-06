2d1c51c06bb831fba5a6855fc5dbaea7c0ab4571
valenyuge <valenn.y@hotmail.com>
2025-06-05 22:37:16 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -22,12 +22,12 @@ let mic;
 let fft;
 
 let frecMinGrave = 40;
-let frecMaxGrave = 700;
+let frecMaxGrave = 800;
 let umbralEnergiaGrave = 140;
 
 let frecMinAgudo = 1500;
 let frecMaxAgudo = 5000;
-let umbralEnergiaAguda = 50;
+let umbralEnergiaAguda = 60;
 
 let umbralAplauso = 0.4;
 let ultimoTiempoAplauso = 0;
